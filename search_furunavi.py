d5bf8905ead59d483f43809973052f921ee9c4f6
YuyaIsh <dame.blazer@gmail.com>
2023-08-21 15:28:51 +0900

    量の取得に失敗した場合もデータ出力するように変更

--- search_furunavi.py
+++ search_furunavi.py
@@ -10,6 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import os
+from datetime import datetime
 
 
 def main():
@@ -37,65 +38,100 @@ def main():
     page_cnt = math.ceil(int(product_cnt.text) / 100)
 
 
-    results = [["商品名","重さ(g)","値段","円/100g","URL",]]
+    results = [["商品名","重さ(g)","値段","円/100g","URL",datetime.now()]]
     for page in range(1,page_cnt+1):
-        print(f"{page}/{page_cnt}ページ")
-        driver.get(target_url+f"&pageno={page}")
-        sleep(1)
+        try:
+            print(f"{page}/{page_cnt}ページ")
+            driver.get(target_url+f"&pageno={page}")
+            sleep(1)
 
-        main_list = driver.find_element(By.CLASS_NAME,"main_list")
-        products = main_list.find_elements(By.CLASS_NAME,"product-info")
+            # ページ内の製品を取得
+            main_list = driver.find_element(By.CLASS_NAME,"main_list")
+            products = main_list.find_elements(By.CLASS_NAME,"product-info")
 
-        for product in products:
-            product_name_ele = product.find_element(By.CLASS_NAME,"product-name")
-            product_name = unicodedata.normalize('NFKC',product_name_ele.text)
-            result = [product_name]
-            g_position = product_name.rfind("g")
-            qty = ""
-            if g_position != -1:
-                for i in range(1,10):
-                    char_to_confirm = product_name[g_position-i]
-                    if char_to_confirm.isdigit() or char_to_confirm == ".":
-                        qty = char_to_confirm + qty
-                    elif char_to_confirm == ",":
-                        pass
-                    elif char_to_confirm == "k":
-                        pass
+            # 製品ごとにデータ取得
+            for product in products:
+                result = []
+
+                # 製品名取得
+                product_name_ele = product.find_element(By.CLASS_NAME,"product-name")
+                product_name = unicodedata.normalize('NFKC',product_name_ele.text)
+                result.append(product_name)
+
+                # 量を取得
+                g_position = product_name.find("g")  # g(グラム)の位置を確認
+                try:
+                    qty = ""
+                    if g_position == -1:
+                        qty = "なし"
                     else:
-                        break
-                if qty:
-                    qty = float(qty)
-                if product_name[g_position-1] == "k":
-                    qty = qty*1000
+                        for i in range(1,10):
+                            char_to_confirm = product_name[g_position-i]
+                            if char_to_confirm.isdigit() or char_to_confirm == ".":
+                                qty = char_to_confirm + qty
+                            elif char_to_confirm == ",":
+                                pass
+                            elif char_to_confirm == "k":
+                                pass
+                            else:
+                                break
+                        if qty:
+                            qty = float(qty)
+                        if product_name[g_position-1] == "k":
+                            qty = qty*1000
 
-                multiplier = ""
-                if len(product_name) > g_position+1:
-                    if product_name[g_position+1] == "×":
-                        for i in range(2,6):
-                            if len(product_name) > g_position+i:
-                                char_to_confirm = product_name[g_position+i]
-                                if char_to_confirm.isdigit():
-                                    multiplier += char_to_confirm
-                                else:
-                                    break
-                        try:
-                            multiplier = float(multiplier)
-                        except Exception as e:
-                            print(e)
-                            print(multiplier)
-                if multiplier:
-                    qty *= multiplier
-            result.append(qty)
-            product_price = int(product.find_element(By.CLASS_NAME,"product-price").text.replace(",",""))
-            result.append(product_price)
-            product_url = product_name_ele.find_element(By.TAG_NAME,"a").get_attribute("href")
-            price_per_100g = product_price/qty * 100 if qty else ""
-            result.append(price_per_100g)
-            result.append(product_url)
-            results.append(result)
+                        multiplier = ""
+                        if len(product_name) > g_position+1:
+                            if product_name[g_position+1] == "×":
+                                for i in range(2,6):
+                                    if len(product_name) > g_position+i:
+                                        char_to_confirm = product_name[g_position+i]
+                                        if char_to_confirm.isdigit():
+                                            multiplier += char_to_confirm
+                                        else:
+                                            break
+                                multiplier = float(multiplier)
+                        if multiplier:
+                            qty *= multiplier
+                except Exception as e:
+                    qty = e
+                    print("qty error")
+                    print(e)
+                    print("qty:",qty)
+                    print("multiplier:",multiplier)
+                result.append(qty)
 
-    with open(results_file_path,'w',newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(results)
+                # 製品の価格を取得
+                product_price = int(product.find_element(By.CLASS_NAME,"product-price").text.replace(",",""))
+                result.append(product_price)
+
+                # 100gあたりの価格を計算
+                try:
+                    price_per_100g = product_price/qty * 100
+                except Exception as e:
+                    price_per_100g = e
+                    print("price_per_100g error")
+                    print(e)
+                result.append(price_per_100g)
+
+                # 製品のURLを取得
+                product_url = product_name_ele.find_element(By.TAG_NAME,"a").get_attribute("href")
+                result.append(product_url)
+
+                results.append(result)
+        except Exception as e:
+            print(e)
+
+    def save_to_csv():
+        with open(results_file_path,'w',newline="",encoding='shift-jis') as f:
+            writer = csv.writer(f)
+            writer.writerows(results)
+    try:
+        save_to_csv()
+    except PermissionError as e:
+        print(f"{results_filename}を閉じてEnterキーを押してください。")
+        print(e)
+        input()
+        save_to_csv()
 
 main()
